8b885133b4f8ff085e4c7c5f986cf6a924f7c913
Aravinthk95 <107410327+Aravinthk95@users.noreply.github.com>
2022-12-21 17:58:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+var maincontainer=document.createElement('div');
+maincontainer.setAttribute('id', 'display');
+
+var parent=document.createElement('div');
+parent.setAttribute('class','disp');
+maincontainer.append(parent);
+
+
+document.body.append(maincontainer);
+
+
+let timer=10;
+let time=document.getElementById('display');
+
+
+setTimeout(() =>{
+    // time.className='disp';
+    time.innerHTML=timer;
+    
+     setTimeout(() =>{
+         time.className='disp';
+         timer=timer-1;
+         time.innerHTML=timer;
+         setTimeout(() =>{
+             time.className='disp';
+             timer=timer-1;
+             time.innerHTML=timer;
+             setTimeout(() =>{
+                 time.className='disp';
+                 timer=timer-1;
+                 time.innerHTML=timer;
+                 setTimeout(() =>{
+                     time.className='disp';
+                     timer=timer-1;
+                     time.innerHTML=timer;
+                     setTimeout(() =>{
+                         time.className='disp';
+                         timer=timer-1;
+                         time.innerHTML=timer;
+                         setTimeout(() =>{
+                             time.className='disp';
+                             timer=timer-1;
+                             time.innerHTML=timer;
+                             setTimeout(() =>{
+                                 time.className='disp';
+                                 timer=timer-1;
+                                 time.innerHTML=timer;
+                                 setTimeout(() =>{
+                                     time.className='disp';
+                                     timer=timer-1;
+                                     time.innerHTML=timer;
+                                     setTimeout(() => {
+                                        time.className='disp';
+                                        timer=timer-1;
+                                        time.innerHTML=timer;
+                                        // setTimeout(() => {
+                                        //     time.className='disp';
+                                        //     timer=timer-1;
+                                        //     time.innerHTML=timer;
+                                         
+                                     
+                                     setTimeout(() =>{
+                                      
+                                         time.className='disp';
+                                         time.innerHTML="HAPPY INDEPENDENCE DAY !"
+                                        //  },1000)
+                                       }, 1000)
+                                    },1000)
+                                 },1000)
+                             },1000)
+                         },1000)
+                     },1000)
+                 },1000)
+             },1000)
+         },1000)
+     },1000)    
+},1000);
